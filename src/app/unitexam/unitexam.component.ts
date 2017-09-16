2c7cb7a33637c92282ffe37ac819f8910a5d0e45
kanyun <949955482@qq.com>
2017-09-16 14:08:55 +0800

    添加一些提示

--- src/app/unitexam/unitexam.component.ts
+++ src/app/unitexam/unitexam.component.ts
@@ -17,22 +17,23 @@ export class UnitexamComponent implements OnInit {
 
 
   public typeCode: string;
-  public Listdata: Array<any>;
-  public pAnswers: Array<any>;
-  public result: any;
+  public Listdata: Array<any>;   //试题集合
+  public totleCount: number;    //加载记录数
+  public pAnswers: Array<any>;  //用户回答
+  public result: any;           //结果，由子组件传递过来
   public isModalShown: boolean = false;
   @ViewChild('autoShownModal')
   public autoShownModal: ModalDirective;
 
-  constructor(private route: ActivatedRoute,
-              private router: Router,
+  constructor(private _route: ActivatedRoute,
+              private _router: Router,
               private _unitexamService: UnitexamService, private _bsModalService: BsModalService) {
     /*通过这种形式来接收父级页面传过来的值  或者通过  this.route.params['value']['typeCode']*/
     /*route.params是一个可观察对象，可以使用.subscribe(),将参数值提取到固定值，这种情况下，我们将params['id'];赋值给组件实例变量id*/
     // route.params与route.queryParams不同,route.params在路由配置中匹配参数，而queryParams在查询字符串中匹配参数
     // this.route.params.subscribe(data=>console.log("传递的参数为："+data.typeCode));
     // 如果没有typeCode参数，那么我们将this.typeCode设置为 空字符串
-    this.route.params.subscribe(params => {
+    this._route.params.subscribe(params => {
       this.typeCode = params['typeCode'] || '';
     });
     this.pAnswers = [];
@@ -41,7 +42,7 @@ export class UnitexamComponent implements OnInit {
 
   ngOnInit() {
     /*订阅事件将路由传递参数值传递给Service*/
-    this.route.params.subscribe((params) => {
+    this._route.params.subscribe((params) => {
       this.typeCode = params['typeCode'] || '';
       this.getUnitExam(this.typeCode);
     });
@@ -51,6 +52,7 @@ export class UnitexamComponent implements OnInit {
   getUnitExam(typeCode: string): any {
     return this._unitexamService.getUnitExam(typeCode).subscribe(res => {
         this.Listdata = res['data'];
+        this.totleCount = res['totalCount'];
         /*从service获取数据，订阅将数据到Component*/
       }, (err) => {
         console.log(`error ${err}`);
@@ -81,10 +83,12 @@ export class UnitexamComponent implements OnInit {
   }
   reviewErrList(examId: number): boolean {
     let falg: boolean = false;
-    let errList = this.result.errorList;
-    if (examId == errList[0]){
-      falg = true;
-    }
+    // console.log("result:  "+this.result);
+    // let errList = this.result.errorList;
+    // if (examId == errList.errorItemId){
+    //   falg = true;
+    // }
     return falg;
   }
+
 }
--- src/app/util/util.component.ts
+++ src/app/util/util.component.ts
@@ -13,8 +13,11 @@ export class UtilComponent implements OnInit {
   /*Input相当于指令的值绑定，无论是单向的(@)还是双向的(=)。都是将父作用域的值“输入”到子作用域中，然后子作用域进行相关处理。
   Output相当于指令的方法绑定，子作用域触发事件执行响应函数，而响应函数方法体则位于父作用域中，相当于将事件“输出到”父作用域中，在父作用域中处理。*/
   @Input() pAnswer;
+  @Input() totleCount;
   /*声明事件发射器*/
   @Output() childResult = new EventEmitter<any>();
+
+  public notDoCount: number;
 
   @ViewChild('staticModal')
   public staticModal: ModalDirective;
@@ -27,6 +30,7 @@ export class UtilComponent implements OnInit {
   ngOnInit() {
   }
 
+  // 提交用户答案到后台
   submitUnitExam(): any {
     this.staticModal.hide();
     console.log(`点击了提交按钮： ` + this.pAnswer);
@@ -44,23 +48,32 @@ export class UtilComponent implements OnInit {
     this.childResult.emit(data);
   }
 
-  /* inquirySubmit(): void {
+  // 还有几道题没有做提示
+  notDo(): boolean{    
+    let falg: boolean = false;
+    this.notDoCount = this.totleCount - this.pAnswer.length;
+    if (this.notDoCount > 0) {
+      falg = true;
+    }
+    return falg
+  }
+
+  // 如果用户一道题都没有做点提交选择不同的弹窗
+  chooseModel(): void{
+    if (this.pAnswer.length == 0){
+      this.tips()
+    }else{
+      this.staticModal.show()
+    }
+  }
+
+   tips(): void {
      swal({
-       title: '确定提交吗',
-       text: "You won't be able to revert this!",
+       title: '提示',
+       text: "至少选择一个答案!",
        type: 'warning',
-       showCancelButton: true,
-       cancelButtonText: '取消',
-       cancelButtonColor: '#d33',
-       confirmButtonColor: '#3085d6',
-       confirmButtonText: '确认提交',
-       showConfirmButton: true
-     }).then(function (isConfirm) {
-       if (isConfirm === true) {
-         //回调可能会报错
-         this.submitUnitExam();
-       }
+       timer: 2000
      })
-   }*/
+   }
 
 }
