fd9edb11155eab126af2355826b1a6f2600f8ef5
zhaoyingxu <949955482@qq.com>
2017-09-26 17:41:53 +0800

    first commit

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ManagerComponent} from "./manager.component";
+import {RouterModule} from "@angular/router";
+import {managerRoutes} from "./manager.routes";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(managerRoutes, {useHash: false}),
   ],
   declarations: [ManagerComponent]
 })
