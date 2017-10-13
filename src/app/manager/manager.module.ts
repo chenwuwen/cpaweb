3f02e3865ba7e09d44906dd98d2b972e846cfdb7
zhaoyingxu <949955482@qq.com>
2017-10-13 23:54:29 +0800

    first commit

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -4,12 +4,14 @@ import {ManagerComponent} from "./manager.component";
 import {RouterModule} from "@angular/router";
 import {managerRoutes} from "./manager.routes";
 import {ItemmanagerModule} from "./itemmanager/itemmanager.module";
+import {DashboardModule} from "./dashboard/dashboard.module";
 
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(managerRoutes), /*子路由使用forChild*/
+    DashboardModule,
     ItemmanagerModule
   ],
   declarations: [ManagerComponent]
--- src/app/manager/manager.routes.ts
+++ src/app/manager/manager.routes.ts
@@ -1,6 +1,7 @@
 import {RouterModule} from '@angular/router';
 import {ItemmanagerComponent} from "./itemmanager/itemmanager.component";
 import {ManagerComponent} from "./manager.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 
 
@@ -8,7 +9,8 @@ export const managerRoutes = [{
   path: 'manager',
   component: ManagerComponent,
   children: [
-    {path: '', redirectTo: 'posttable/page/1', pathMatch: 'full'}, /*写入二级路由forChild，此处必不可少的是path的空白链接*/
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'}, /*写入二级路由forChild，此处必不可少的是path的空白链接*/
+    {path: 'dashboard', component: DashboardComponent},
     {path: 'addExam', component: ItemmanagerComponent}
 
   ]
