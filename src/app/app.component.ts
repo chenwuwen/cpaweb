888e44165cc45d8ac4220167111aaee52f81906d
kanyun <949955482@qq.com>
2017-11-25 16:43:33 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';  /*所有组件必须引入*/
+import { Component, ViewChild } from '@angular/core';  /*所有组件必须引入*/
 import { ActivatedRoute, Router, ActivatedRouteSnapshot, RouterState, RouterStateSnapshot } from '@angular/router';
+import { ElementRef } from '@angular/core/src/linker/element_ref';
+import { LoginmodelComponent } from './common/loginmodel/loginmodel.component';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +10,27 @@ import { ActivatedRoute, Router, ActivatedRouteSnapshot, RouterState, RouterStat
   styleUrls: ['./app.component.css']   /*HTML模板使用的样式表，可以有多个。*/
 })
 export class AppComponent {
+  @ViewChild('loginModal')
+  private loginModal: LoginmodelComponent;
+  public userName: string = "请登陆";
 
   constructor(
     // 在constructor中注入的依赖，就可以作为类的属性被使用了
     public router: Router,
-    public activatedRoute: ActivatedRoute
-  ){
+    public activatedRoute: ActivatedRoute,
+    public appService: AppService
+  ) {
 
   }
 
+  private login() {
+    this.loginModal.showLoginModal();
+    return;
+  }
+
+  private logout(): void {
+    console.log(`用户注销`);
+    this.appService.logout()
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
-import {BrowserModule} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 /*每个在浏览器中运行的应用的根模块都需要引入BrowserModule*/
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 /*每个模块都需要引入的核心库中的NgModule*/
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 /*表单模块，在应用中使用表单时引入*/
-import {HttpModule} from '@angular/http';
+import { HttpModule } from '@angular/http';
 /*http模块，当需要进行http远程请求时引入*/
-import {Ng2BootstrapModule} from 'ngx-bootstrap';
-import {RouterModule} from '@angular/router';
+import { Ng2BootstrapModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 /*路由模块*/
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
 /*该模块是包含一些常用内置指令模块，如*ngFor*/
 
 
-import {AppComponent} from './app.component';
-import {appRoutes} from './app.routes';
-import {UnitexamModule} from "./unitexam/unitexam.module"
-import {HomeModule} from "./home/home.module";
-import {ManagerModule} from "./manager/manager.module";
-import {ItemmanagerModule} from "./manager/itemmanager/itemmanager.module";
-import {UsercenterModule} from './usercenter/usercenter.module';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { UnitexamModule } from "./unitexam/unitexam.module"
+import { HomeModule } from "./home/home.module";
+import { ManagerModule } from "./manager/manager.module";
+import { ItemmanagerModule } from "./manager/itemmanager/itemmanager.module";
+import { UsercenterModule } from './usercenter/usercenter.module';
+import { LoginmodelModule } from './common/loginmodel/loginmodel.module';
+import { AppService } from './app.service';
 
 @NgModule({
   declarations: [/*声明属于本模块的组件，每个组件(管道)必须在且仅在一个模块中声明,g*/
@@ -35,15 +37,16 @@ import {UsercenterModule} from './usercenter/usercenter.module';
     /*angular2默认采用HTML5的pushState来管理路由，它会导致前端路由与后端路由的冲突，例如当部署到nginx环境时，
     我们通过首页进入子路由一切正常，但是在子路由路径下，刷新就会报404了。默认情况下nginx会当成这个路径是实际web路径下的资源而去定位它，
     但可想而知实际是并不存在的。折中的方案可以改回hash风格*/
-    RouterModule.forRoot(appRoutes, {useHash: false}), /*angular2项目中url去掉#(即哈希路由)*/
+    RouterModule.forRoot(appRoutes, { useHash: false }), /*angular2项目中url去掉#(即哈希路由)*/
     UnitexamModule,
     UsercenterModule,
     HomeModule,
     ManagerModule,
     ItemmanagerModule,
+    LoginmodelModule,
   ],
   providers: [/*声明模块中使用的服务的提供者,对于appModule来说,也应该就是控制菜单显示了，即权限控制*/
-
+    AppService
   ],
   bootstrap: [/*根模块中的引导组件，应用启动过程中，会创建这个数组中的组件并插入到HTML中，一般只有一个引导组件*/
     AppComponent
